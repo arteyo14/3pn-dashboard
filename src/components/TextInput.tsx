9542c9e024afdc2c9cb4af83c2f03bb1d2771e64
arteyo <pitchayakorn.s@komgrip.com>
2025-02-13 11:29:43 +0700

    [text input] fix: text input component

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -2,17 +2,32 @@
 
 import Label from "./Label"
 
-interface Props {
+interface IProps {
   label: string
   name: string
-  required: boolean
+  required?: boolean
+  type?: string
+  className?: string
+  onChange?: (value: string) => void
 }
 
-export default function TextInput({ label = "", name = "", required }: Props) {
+export default function TextInput({
+  label = "",
+  name = "",
+  required,
+  type,
+  className,
+  onChange,
+}: IProps) {
   return (
-    <div className="gap-1 flex flex-col max-h-[50px]">
-      <Label label={label} required={required} />
-      <input type="text" name={name} />
+    <div className={`gap-1 flex flex-col ${className}`}>
+      <Label label={label} required={required ?? false} />
+      <input
+        type={type}
+        name={name}
+        className="input input-bordered w-full bg-white"
+        onChange={(event) => onChange?.(event.target.value)}
+      />
     </div>
   )
 }
--- src/modules/auth/login/ui/LoginCard.tsx
+++ src/modules/auth/login/ui/LoginCard.tsx
@@ -7,13 +7,14 @@ export default function LoginCard() {
     <div className="card card-compact min-w-[350px] max-w-[450px] shadow-lg">
       <div className="card-body flex flex-col justify-center items-center">
         <span className="card-title text-center text-black">เข้าสู่ระบบ</span>
-        <form className="text-center my-4 px-4">
-          <TextInput label="ชื่อผู้ใช้" required={true} name="username" />
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered w-full "
+        <form className="text-center my-4 px-4 w-full">
+          <TextInput
+            label="ชื่อผู้ใช้"
+            name="username"
+            className="mb-2"
+            required={true}
           />
+          <TextInput label="รหัสผ่าน" name="password" required={true} />
         </form>
         <div className="w-full text-end">
           <a href="#" className="text-primary hover:underline">
