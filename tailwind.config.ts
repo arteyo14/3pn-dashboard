79911101786cf5917162c36505e6ad1c79fdb457
arteyo <pitchayakorn.s@komgrip.com>
2025-02-14 21:30:48 +0700

    [tailwind] set: primary & secondary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#0D66DA",
+        secondary: "#fff",
       },
     },
   },
